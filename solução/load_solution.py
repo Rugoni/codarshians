ff352e4fd843c5be8990e67e6714973d450bb215
Alice Costa <atr@certi.org.br>
2021-09-25 00:27:40 -0300

    Adiciona exportação dos dados de resposta.

--- solução/load_solution.py
+++ solução/load_solution.py
@@ -1,8 +1,7 @@
-
-from pergunta3 import resolve_pergunta_3
-from pergunta2 import resolve_pergunta_2
 import pandas as pd
 from pergunta1 import resolve_pergunta_1
+from pergunta2 import resolve_pergunta_2
+from pergunta3 import resolve_pergunta_3
 from pergunta4 import resolve_pergunta_4
 from pergunta5 import resolve_pergunta_5
 from pergunta6 import resolve_pergunta_6
@@ -14,24 +13,44 @@ simples = pd.read_feather(path+'simples.ftr')
 empresas = pd.read_feather(path+'empresas.ftr')
 estabelecimentos = pd.read_csv(path+'estabelecimentos.csv', sep = ';')
 estabelecimentos['Data Situação Cadastral'] = pd.to_datetime(estabelecimentos['Data Situação Cadastral'], exact=False, format='%Y-%m-%d')
-# #estabelecimentos['Data de Início atividade'] = pd.to_datetime(estabelecimentos['Data de Início atividade'], exact=False, format='%YYYY-%mm-%dd %HH:%MM:%SS')
-# print(type(estabelecimentos['Data Situação Cadastral'][0]))
-# print(type(estabelecimentos['Data de Início atividade'][0]))
 
-# print("- Solucionando 1")
-# industrias_ativas_simples, industrias_ativas_mei = resolve_pergunta_1(municipios, estabelecimentos, simples)
+print("- Solucionando 1")
+industrias_ativas_simples, industrias_ativas_mei = resolve_pergunta_1(municipios, estabelecimentos, simples)
 
-# print("- Solucionando 2")
-# comercios_inativos_simples, comercios_inativos_mei = resolve_pergunta_2(municipios, estabelecimentos, simples)
+print("- Solucionando 2")
+comercios_inativos_simples, comercios_inativos_mei = resolve_pergunta_2(municipios, estabelecimentos, simples)
 
-# print("- Solucionando 3")
-# novos_simples, novos_mei = resolve_pergunta_3(municipios, estabelecimentos, simples)
+print("- Solucionando 3")
+novos_simples, novos_mei = resolve_pergunta_3(municipios, estabelecimentos, simples)
 
-# print("- Solucionando 4")
-# novos_ed_superior = resolve_pergunta_4(estabelecimentos)
+print("- Solucionando 4")
+novos_ed_superior = resolve_pergunta_4(estabelecimentos)
 
-# print("- Solucionando 5")
-# capital_social_médio = resolve_pergunta_5(estabelecimentos, empresas)
+print("- Solucionando 5")
+capital_social_médio = resolve_pergunta_5(estabelecimentos, empresas)
 
 print("- Solucionando 6")
 capital_nat_juridica = resolve_pergunta_6(estabelecimentos, empresas)
+
+print("-- Exportando os arquivos:")
+path_save = '/home/atr/codarshians/codarshians/respostas/'
+industrias_ativas_simples.to_csv(path_save+'industrias_ativas_simples.csv', index=False, header=True, sep=';')
+industrias_ativas_mei.to_csv(path_save+'industrias_ativas_mei.csv', index=False, header=True, sep=';')
+print("Pergunta 1 - OK!")
+
+comercios_inativos_simples.to_csv(path_save+'comercios_inativos_simples.csv', index=False, header=True, sep=';')
+comercios_inativos_mei.to_csv(path_save+'comercios_inativos_mei.csv', index=False, header=True, sep=';')
+print("Pergunta 2 - OK!")
+
+novos_simples.to_csv(path_save+'novos_simples.csv', index=False, header=True, sep=';')
+novos_mei.to_csv(path_save+'novos_mei.csv', index=False, header=True, sep=';')
+print("Pergunta 3 - OK!")
+
+novos_ed_superior.to_csv(path_save+'novos_ed_superior.csv', index=False, header=True, sep=';')
+print("Pergunta 4 - OK!")
+
+capital_social_médio.to_csv(path_save+'capital_social_médio.csv', index=False, header=True, sep=';')
+print("Pergunta 5 - OK!")
+
+capital_nat_juridica.to_csv(path_save+'capital_nat_juridica.csv', index=False, header=True, sep=';')
+print("Pergunta 6 - OK!")
